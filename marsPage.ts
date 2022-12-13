fad41d832f92b55f06db048de4673d7b04649b4b
Marohn Howard <mars.howard@devmounta.in>
2022-12-12 19:11:07 -0700

    last push

--- marsPage.ts
+++ marsPage.ts
@@ -2,5 +2,32 @@ import {BasePage} from './basePage'
 import {By} from 'selenium-webdriver'
 
 export class Mars extends BasePage {
-    
+    //selectors - what we are locating/ manipulating
+    englishBtn: By = By.css('#langSelect-EN')
+    bigCookie: By = By.xpath('//button[@id = "bigCookie"]')
+    legacyBtn: By = By.xpath('//div[text() = "Legacy"]')
+    ascendBtn: By = By.xpath('//a[@class = "option smallFancyButton focused"]')
+    reincarnateBtn: By = By.css('#ascendButton')
+    yesBtn: By = By.xpath('//a[text() = "Yes"]')
+    twitterBtn: By = By.xpath('//a[text()="twitter"]')
+    cusorBtn: By = By.xpath('//div[@id = "productIcon0"]')
+    constructor() {
+        super({url: "https://orteil.dashnet.org/cookieclicker/"})
+    }
+
+    //methods - repeatable functionality 
+
+    async repeatClick(clickNum, elementClick) {
+        for (let i = 0; i < clickNum; i++) {
+            await this.click(elementClick)
+        }
+    }
+
+    async tabs() {
+        let myTabs = await this.driver.getAllWindowHandles()
+        await this.driver.switchTo().window(myTabs[1]);
+        await this.driver.sleep(1000);
+        await this.driver.close()
+        await this.driver.switchTo().window(myTabs[0])
+    }
 }
--- marsTest.test.ts
+++ marsTest.test.ts
@@ -1,3 +1,23 @@
 import {Mars} from './marsPage'
 const mars = new Mars()
 
+describe('Mars Solo Capstone Cookie Clicker Project', () => {
+    test('Can click the cookie a lot and switch tabs', async () => {
+        await mars.navigate()
+        await mars.click(mars.englishBtn)
+        await mars.driver.sleep(3000)
+        await mars.repeatClick(200, mars.bigCookie)
+        await mars.driver.sleep(2000)
+        //await mars.click(mars.cusorBtn)
+        await mars.click(mars.twitterBtn)
+        await mars.tabs()
+        await mars.repeatClick(50, mars.bigCookie)
+        await mars.click(mars.legacyBtn)
+        await mars.click(mars.ascendBtn)
+        await mars.click(mars.reincarnateBtn)
+        await mars.click(mars.yesBtn)
+        await mars.repeatClick(100, mars.bigCookie)
+        await mars.driver.sleep(3000)
+        await mars.driver.quit()
+    })
+})
